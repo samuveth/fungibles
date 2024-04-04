460ca78e37ce3ebfd0471895207fe217b242f7a5
Sam <samuv.eth@gmail.com>
2024-04-04 21:25:57 +0700

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import DaisyUIPlugin from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [DaisyUIPlugin]
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,42 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          customCollections: ['icon'],
+          alias: {
+            hi: 'heroicons'
+          }
+        })
+      ]
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dirs: ['./src/composables'],
+      eslintrc: {
+        enabled: true
+      }
+    }),
+    Icons({
+      compiler: 'vue3',
+      customCollections: {
+        icon: FileSystemIconLoader('./src/assets/icons', (svg) =>
+          svg.replace(/^<svg /, '<svg fill="currentColor" ')
+        )
+      }
+    })
   ],
   resolve: {
     alias: {
