78a959ad60cecc325854b6e1c4ec4717de641b8a
Sam <samuv.eth@gmail.com>
2024-04-16 00:35:01 +0700

    Fix layout

--- src/components/InscriptionList.vue
+++ src/components/InscriptionList.vue
@@ -8,12 +8,12 @@ defineProps<{
 
 <template>
   <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
-    <div v-for="(inscription, i) in inscriptions" :key="i">
+    <template v-for="(inscription, i) in inscriptions" :key="i">
       <InscriptionListItem
         v-if="inscription.svg"
         :inscription="inscription"
         :is-only-inscription="inscriptions.length < 2"
       />
-    </div>
+    </template>
   </div>
 </template>
